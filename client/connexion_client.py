b2ff3b4b3660442f04f5330738a09c6405236bc9
bBibyk <bbibyk@gmail.com>
2024-04-16 00:43:17 +0200

    beginning connexion client

--- client/connexion_client.py
+++ client/connexion_client.py
@@ -1,16 +1,22 @@
 import socket
 import time
+import yaml
 
 class Connexion:
     def __init__(self):
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__socket.bind(("localhost", 4000))
-        self.__socket.connect(("localhost", 4202))
-        self.__socket.sendall(b"Hello")
-        print("Data sent")
-        data = self.__socket.recv(1024)
-        print("Received", repr(data))
-        self.__socket.close() ### Envoi-t-il un message de fin ?
+        pass
+        # self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.__socket.bind(())
+        # self.__socket.connect(("localhost", 4202))
+        # self.__socket.sendall(b"Hello")
+        # print("Data sent")
+        # data = self.__socket.recv(1024)
+        # print("Received", repr(data))
+        # self.__socket.close() ### Envoi-t-il un message de fin ?
+
+    def load_configurations(self):
+        configurations = yaml.load(open("config.yaml", "r"), Loader=yaml.SafeLoader)
+        return configurations
 
     def connect(self):
         # TODO
@@ -23,3 +29,4 @@ class Connexion:
 if __name__ == "__main__":
     print("Test class Connexion")
     connexion = Connexion()
+    print(connexion.load_configurations())
