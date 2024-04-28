e8e2b523453f3f38ecfc7d4d843f987462f29aad
bBibyk <125393249+bBibyk@users.noreply.github.com>
2024-04-28 22:02:08 +0200

    Update main.py

--- module/main.py
+++ module/main.py
@@ -1,11 +1,39 @@
 import module.connexion_server as cs 
 import module.sensor as ss
+from utils import *
 
+def sigint_handler(sig, frame):
+    logger("Main", "Exiting...")
+    connection.disconnect()
+    # TODO : sortir correctement
+    
+def send_photo(filename : str):
+    #TODO
+    pass
+
+def get_configurations():
+    #TODO
+    pass
+
+def get_time():
+    #TODO
+    pass
+
+# Entity creation
 connection = cs.Connexion()
-configs = connection.recv_configurations()
-pc_time = connection.recv_time()
 sensor = ss.Sensor(configurations=configs, pc_time=pc_time)
+#Handle signal
+
+#While loop TODO
+connection.connect()
+configs = connection.recv_configurations()
+connection.disconnect_client()
+
+#for i in range(config_update_frequency):
+connection.connect()
+pc_time = connection.recv_time()
+connection.disconnect_client()
+#TODO reste du script...
 
 sensor.start_capture()
 #send_photo('./shots/*')
-
