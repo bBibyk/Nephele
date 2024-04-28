d8e52ee621661ca7298ec08b35ab83b7a7dfb117
nije <yanis.yzr@gmail.com>
2024-04-28 22:37:42 +0200

    sensor tests

--- module/sensor.py
+++ module/sensor.py
@@ -5,7 +5,7 @@ from os.path import join
 class Sensor:
     def __init__(self, configurations, pc_time : time):
         self.update_configurations(configurations)
-        self.sync_time(pc_time)
+        self.__sync_time(pc_time)
         self.picam2=Picamera2()
     
     def update_configurations(self, configurations):
@@ -17,7 +17,7 @@ class Sensor:
         else:
             logger("Sensor","No configurations provided.")
     
-    def sync_time(self, pc_time):
+    def __sync_time(self, pc_time):
         self.module_time : time = pc_time
         
     def start_preview(self):
@@ -27,7 +27,9 @@ class Sensor:
         self.picam2.stop_preview()
     
     def quick_capture(self):
-        self.picam2.start_and_capture_file("Test.jpg")
+        image_name = "Test.jpg"
+        self.picam2.start_and_capture_file(image_name)
+        logger("Sensor", f"Image captured: {image_name}")
         
     def __name_image(self):
         
@@ -35,26 +37,38 @@ class Sensor:
         image_name_format : str = self.configurations['module']['image_name_format']
         image_name : str = image_name_format.format(timestamp=timestamp)  
         return image_name
-         
-    def capture_file(self):
+        
+    def capture_image(self):
             try:
-                with self.picam2.start_preview():
+                with self.start_preview():
                     
                     image_name = self.__name_image()
-                    image_path = join(self.configurations['module']['shots'], image_name)
+                    image_path = image_name + get_script_directory()+self.configurations['module']['shots']
+                    # image_path = join(self.configurations['module']['shots'], image_name)
                     with self.picam2.capture_file(image_path):
                         logger("Sensor", f"Image captured: {image_name}")
+                    self.stop_preview()
                     
             except Exception as e:
                 logger("Sensor", "Error during capture.", e)
+                self.stop_preview()
 
     def check_brightness(self):
         pass
 
 if __name__ == '__main__':
-    from picamera2 import Preview
+    
+    import time
+    
+    configurations = load_configurations("default.yaml")
+    test_time = time.time()
+    
+    
     print("Test quick capture.")
-    sensor = Sensor()
+    
+    sensor = Sensor(configurations=configurations, time=test_time)
     sensor.quick_capture()
 
+    print("Test classic capture")
+    sensor.capture_image()
     
