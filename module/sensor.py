600007540b5d6d0525ab5ef7e6ea6e1c826e7f85
nije <yanis.yzr@gmail.com>
2024-05-04 00:35:55 +0200

    preparing night mode

--- module/sensor.py
+++ module/sensor.py
@@ -1,9 +1,11 @@
 from picamera2 import Picamera2, Preview
 from utils import *
 from time import sleep
+from os import remove
 
 class Sensor:
     def __init__(self, configurations, pc_time : time):
+        self.metadata = None
         self.update_configurations(configurations)
         self.sync_time(pc_time)
         self.picam2=Picamera2()
@@ -17,14 +19,12 @@ class Sensor:
         else:
             logger("Sensor","No configurations provided.")
     
-    def sync_time(self, pc_time : time):
+    def sync_time(self, pc_time : time):    
         self.module_time = time.localtime(pc_time)
         
     def start_preview(self):
-            
-        self.config = self.picam2.create_preview_configuration()
-        self.picam2.configure(self.config)
-        self.picam2.start_preview(Preview.QTGL)
+        
+        self.picam2.start_preview(Preview.NULL)
         self.picam2.start()
         sleep(1)
 
@@ -35,27 +35,40 @@ class Sensor:
         image_name = "Test.jpg"
         self.picam2.start_and_capture_file(image_name)
         logger("Sensor", f"Image captured: {image_name}")
+        self.stop()
         
-    def __name_image(self):
+    def __get_path(self):
         
         timestamp : str = time.strftime("%Y%m%d%H%M%S", self.module_time)
         image_name_format : str = self.configurations['module']['format']['image_name']
-        image_name : str = image_name_format.format(timestamp=timestamp)  
-        return image_name
+        image_name : str = image_name_format.format(timestamp=timestamp)
+        image_path = get_script_directory()+self.configurations['module']['shots'] + image_name  
+        return image_path, image_name
         
     def capture_image(self):
-            try:    
-                image_name = self.__name_image()
-                image_path = get_script_directory()+self.configurations['module']['shots'] + image_name
-                self.picam2.capture_file(image_path)
-                logger("Sensor", f"Image captured: {image_name}")
-            
-            except Exception as e:
-                logger("Sensor", "Error during capture.", e)
-                self.stop()
+        
+        image_path, image_name = self.__get_path()
+        try:    
+            self.metadata = self.picam2.capture_file(image_path)
+            logger("Sensor", f"Image captured: {image_name}")
+        
+        except Exception as e:
+            logger("Sensor", "Error during capture.", e)
+            self.stop()
 
     def check_brightness(self):
+        if self.metadata is not None:
+            brightness_threshold = self.configurations['sensor']['brightness_threshold']
+            if self.metadata['Lux']<brightness_threshold:
+                return False
+        return True
+    
+    def __night_mode(args):
         pass
+    
+    def enable_night_mode(args):
+        pass
+                
     
     def stop(self):
         self.picam2.close()
