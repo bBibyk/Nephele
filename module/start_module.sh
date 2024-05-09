f27d1ddce11796b1c418c0e74665987a8d1a8569
bBibyk <bbibyk@gmail.com>
2024-05-10 01:30:37 +0200

    autostart&keepalive script added

--- module/start_module.sh
+++ module/start_module.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/bash
-python /home/Nephele/meteo_module/module/main.py
+
+PROCESS_NAME="python3 ./connexion_server.py"
+
+while true
+do
+    if ! pgrep -f "$PROCESS_NAME" > /dev/null
+    then
+    	echo "$PROCESS_NAME n'est pas en cours d'exécution. Redémarrage..."
+        python3 ./connexion_server.py   
+    fi
+    sleep 5
+done
