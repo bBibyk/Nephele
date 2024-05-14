403241bd0a3b871208bc851b9d048151b409657f
nije <yanis.yzr@gmail.com>
2024-05-15 00:28:16 +0200

    Added sigint handler

--- client/main.py
+++ client/main.py
@@ -1,9 +1,19 @@
 import connexion_client as cc
+import signal
 from utils import *
 import time
 
+def sigint_handler(sig, frame):
+    global connexion
+    logger("Main", "Exiting...")
+    connexion.disconnect()
+    exit()
+
+signal.signal(signal.SIGINT, sigint_handler)
+    
 connexion = cc.Connexion()
 connected = False
+
 
 while True :
     try:
